ffc4b7be449b2b2cd6dcfce9d514d48f9a196143
Nilesh Jain <neeljain7821@gmail.com>
2023-12-29 13:26:36 +0530

    added the plugin and the pre hook in mongoose

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -46,5 +48,16 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export const User = model("User", userSchema);
